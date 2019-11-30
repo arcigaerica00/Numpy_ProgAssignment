78ddd6c5d677f595e669afae7ced22d8ba71a9fc
arcigaerica00 <57585402+arcigaerica00@users.noreply.github.com>
2019-11-30 21:21:10 +0800

    Delete NUMPY_divby3.py

--- NUMPY_divby3.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-A=np.linspace(1,100,100)
-B=A.reshape(10,10)
-C=B**2
-D=C[(C%3)==0]
-print('These are the elements divisible by 3 from the matrix of the squares of the first 100 positive integers')
-print(D)
-np.save('div_by_3', D)
